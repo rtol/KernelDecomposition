eec3d09b057eb48b640ab0303a46cad97d743328
Richard S.J. Tol <r.tol@sussex.ac.uk>
2021-04-11 11:17:56 +0100

    Add files via upload

--- ReadEstimates.m
+++ ReadEstimates.m
@@ -7,25 +7,25 @@ display('Read data');
 
 vFileToOpen = 'socialcostcarbon.xlsx';
 %data
-SCC = xlsread(vFileToOpen, 'Data', 'L9:L5780');
-Growth = xlsread(vFileToOpen, 'Growth', 'G9:G5780');
+SCC = xlsread(vFileToOpen, 'Data', 'L9:L5799');
+Growth = xlsread(vFileToOpen, 'Growth', 'G9:G5799');
 %study characteristics
-GWeight = xlsread(vFileToOpen, 'Growth', 'D9:D5780');
-Year = xlsread(vFileToOpen, 'Data', 'D9:D5780');
-Censor = xlsread(vFileToOpen, 'Data', 'H9:H5780');
-PaperWeight = xlsread(vFileToOpen, 'Data', 'K9:K5780');
-AuthorWeight = xlsread(vFileToOpen, 'Data', 'J9:J5780');
-TotalWeight= xlsread(vFileToOpen, 'Data', 'I9:I5780');
-Peer = xlsread(vFileToOpen, 'Data', 'Q9:Q5780');
-PRTP = xlsread(vFileToOpen, 'Data', 'AE9:AE5780');
-CDR = xlsread(vFileToOpen, 'Data', 'AD9:AD5780');
-EquityWeight = xlsread(vFileToOpen, 'Data', 'AF9:AF5780');
-Expectation = xlsread(vFileToOpen, 'Data', 'AG9:AG5780');
-Hope = xlsread(vFileToOpen, 'Data', 'AH9:AH5780');
-Nordhaus = xlsread(vFileToOpen, 'Data', 'AI9:AI5780');
-Tol = xlsread(vFileToOpen, 'Data', 'AJ9:AJ5780');
-Ploeg = xlsread(vFileToOpen, 'Data', 'AK9:AK5780');
-Pigou = xlsread(vFileToOpen, 'Data', 'AM9:AM5780');
-Neg = xlsread(vFileToOpen, 'Data', 'W9:W5780');
+GWeight = xlsread(vFileToOpen, 'Growth', 'D9:D5799');
+Year = xlsread(vFileToOpen, 'Data', 'D9:D5799');
+Censor = xlsread(vFileToOpen, 'Data', 'H9:H5799');
+PaperWeight = xlsread(vFileToOpen, 'Data', 'K9:K5799');
+AuthorWeight = xlsread(vFileToOpen, 'Data', 'J9:J5799');
+TotalWeight= xlsread(vFileToOpen, 'Data', 'I9:I5799');
+Peer = xlsread(vFileToOpen, 'Data', 'Q9:Q5799');
+PRTP = xlsread(vFileToOpen, 'Data', 'AE9:AE5799');
+CDR = xlsread(vFileToOpen, 'Data', 'AD9:AD5799');
+EquityWeight = xlsread(vFileToOpen, 'Data', 'AF9:AF5799');
+Expectation = xlsread(vFileToOpen, 'Data', 'AG9:AG5799');
+Hope = xlsread(vFileToOpen, 'Data', 'AH9:AH5799');
+Nordhaus = xlsread(vFileToOpen, 'Data', 'AI9:AI5799');
+Tol = xlsread(vFileToOpen, 'Data', 'AJ9:AJ5799');
+Ploeg = xlsread(vFileToOpen, 'Data', 'AK9:AK5799');
+Pigou = xlsread(vFileToOpen, 'Data', 'AM9:AM5799');
+Neg = xlsread(vFileToOpen, 'Data', 'W9:W5799');
 
 clear v*
